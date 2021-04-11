046e3bb5c66ac50e21ea07d68b9e207eb23d87c1
Firstname Lastname <jmbwebco@gmail.com>
2021-04-11 07:27:10 +0100

    adding faq page

--- app.js
+++ app.js
@@ -4,3 +4,19 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 toggleButton.addEventListener('click', () => {
 	navbarLinks.classList.toggle('active');
 })
+
+//faq accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
